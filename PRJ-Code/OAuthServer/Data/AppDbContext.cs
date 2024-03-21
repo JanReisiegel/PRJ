7c7f299df8928cbdcfcafe544fe6c0d3259c2358
Jan_Reisiegel <jan.reisiegel@tul.cz>
2024-03-21 18:42:25 +0100

    Program.cs some changes

---
+++ PRJ-Code/OAuthServer/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OAuthServer.Models;
+
+namespace OAuthServer.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            //customize the schema here
+        }
+    }
+}
--- PRJ-Code/OAuthServer/Program.cs
+++ PRJ-Code/OAuthServer/Program.cs
@@ -1,7 +1,55 @@
+using IdentityServer4.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Internal;
+using OAuthServer.Data;
+using OAuthServer.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("Users")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+var configurationString = builder.Configuration.GetConnectionString("Configuration");
+
+builder.Services.AddIdentityServer(options =>
+    {
+        options.Events.RaiseErrorEvents = true;
+        options.Events.RaiseInformationEvents = true;
+        options.Events.RaiseFailureEvents = true;
+        options.Events.RaiseSuccessEvents = true;
+
+        options.UserInteraction = new UserInteractionOptions
+        {
+            LoginUrl = "/Account/Login",
+            LogoutUrl = "/Account/Logout",
+            ConsentUrl = "/Consent/Index",
+            ErrorUrl = "/Home/Error",
+        };
+    })
+    .AddAspNetIdentity<ApplicationUser>()
+    .AddConfigurationStore(options =>
+    {
+        options.ConfigureDbContext = db => db.UseSqlite(configurationString);
+    })
+    .AddOperationalStore(options =>
+    {
+        options.ConfigureDbContext = db =>
+        db.UseSqlite(configurationString);
+
+        // this enables automatic token cleanup. this is optional.
+        //options.EnableTokenCleanup = true;
+        // options.TokenCleanupInterval = 15; // interval in seconds. 15 seconds useful for debugging
+    }); ;
 
 var app = builder.Build();
 
